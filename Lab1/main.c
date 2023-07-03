ff0e3ab5a7866e433fa8d2b1b67a94b771933cc7
AirHeus <daniyl.karyuk2016a@gmail.com>
2023-07-03 15:04:57 +0300

    1

--- Lab1/main.c
+++ Lab1/main.c
@@ -4,16 +4,16 @@ int main() {
     int t1, t2, t3;
     double total_time;
 
-    // «читуЇмо вх≥дн≥ дан≥
+    // Read the input data
     scanf("%d %d %d", &t1, &t2, &t3);
 
-    // ќбчислюЇмо суму часу
+    // Calculate the sum of time
     total_time = t1 + t2 + t3;
 
-    // ќкруглюЇмо до 2 знак≥в п≥сл€ коми
+    // Round to 2 decimal places
     total_time = (int)(total_time * 100 + 0.5) / 100.0;
 
-    // ¬иводимо результат
+    // Displaying the result
     printf("%.2f\n", total_time);
 
     return 0;
